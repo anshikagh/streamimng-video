56b0b933389675b59c928268454c39087e9924ec
Anshika Yadav <113241528+anshikagh@users.noreply.github.com>
2023-08-18 11:03:10 +0530

    Add files via upload

---
+++ youtube clone/html/script.js
@@ -0,0 +1,9 @@
+var menuIcon=document.querySelector(".menu-icon");
+var sidebar=document.querySelector(".sidebar");
+ 
+menuIcon.onclick=function(){
+    sidebar.classList.toggle("small-sidebar");
+    
+
+
+}
